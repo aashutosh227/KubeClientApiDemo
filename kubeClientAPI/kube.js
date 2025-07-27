7c686c99fb1e462922365fbfdba49f6350f51015
Aashutosh Singh <aashutoshsingh126@gmail.com>
2025-07-28 01:00:57 +0530

    added namespace read and creation functions

--- kubeClientAPI/kube.js
+++ kubeClientAPI/kube.js
@@ -5,10 +5,52 @@ async function initializeKubeClient() {
 
     const k8sApi = kc.makeApiClient(k8s.CoreV1Api);
 
-    k8sApi.listNamespacedPod({ namespace: 'default' }).then((res) => {
-    console.log(res);
-});
+    return k8sApi;
+};
+
+async function printNameSpacedPods(k8sApi) {
+    await k8sApi.listNamespacedPod({namespace: 'default'}).then((res)=>{
+        const pods = res.items;
+        pods.forEach((pod)=>{
+            console.log("Pod: "+ pod.metadata.name);
+        });
+    });
 }
 
-initializeKubeClient();
+async function createNameSpace(k8sApi, namespace) {
+
+    await k8sApi.createNamespace({ body: namespace }).then(
+    (response) => {
+        console.log('Created namespace');
+        console.log(response);
+        k8sApi.readNamespace(namespace.metadata.name).then((response) => {
+            console.log(response);
+            k8sApi.deleteNamespace(namespace.metadata.name, {} );
+        });
+    },
+    (err) => {
+        console.log('Error!: ' + err);
+    },
+);
+}
+
+
+const main = () => {
+    initializeKubeClient().then(async (k8sApi)=>{
+        console.log("Namesspace before creation: ");
+        await printNameSpacedPods(k8sApi);
+        var namespace = {
+            metadata: {
+                name: 'test',
+            },
+        };
+        await createNameSpace(k8sApi, namespace);
+        console.log("Namesspace after creation: ");
+        await printNameSpacedPods(k8sApi);
+    });
+};
+
+main();
+
+
 
